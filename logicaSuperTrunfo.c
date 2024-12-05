e5e410bffa3898f28367eb04f43f1b1326604058
Sillas04 <sillas.lima16@outlook.com>
2024-12-05 13:55:37 +0000

    Calculo Densidade/Capta e exibição dos dados

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -11,15 +11,15 @@ int main() {
 
     // variaveis para dados da cidade
     char cidade_a01[30], cidade_a02[30],cidade_b01[30], cidade_b02[30];
-    char codigo_a01[3], codigo_a02[4], codigo_b01[3], codigo_b02[4];     
+    char codigo_a01[4], codigo_a02[4], codigo_b01[4], codigo_b02[4];     
     
     // Variaveis para propriedades da cidade
-    int populacao_a01, populacao_a02, populacao_b01, populacao_b02;
+    unsigned long int populacao_a01, populacao_a02, populacao_b01, populacao_b02;
     int turistico_a01, turistico_a02, turistico_b01, turistico_b02;
     float km_area_a01, km_area_a02, km_area_b01, km_area_b02;
     float pib_a01, pib_a02, pib_b01, pib_b02;
 
-    //Variaveis para densidade e pib capta
+    // Variaveis para densidade e pib capta
     float densidade_a01, densidade_a02, densidade_b01, densidade_b02;
     float pib_capta_a01, pib_capta_a02, pib_capta_b01, pib_capta_b02;
 
@@ -37,13 +37,17 @@ int main() {
     printf("Digite o código da cidade: \n");
     scanf("%s", codigo_a01);
     printf("Digite a população: \n");
-    scanf("%d", &populacao_a01);
+    scanf("%lu", &populacao_a01);
     printf("Digite a área em Km²: \n");
     scanf("%f", &km_area_a01);
     printf("Digite o PIB: \n");
     scanf("%f", &pib_a01);
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &turistico_a01);   
+    scanf("%d", &turistico_a01);
+
+    //lógica para densidade e pib   
+    densidade_a01 = (float) populacao_a01 / km_area_a01;
+    pib_capta_a01 = (float) pib_a01 / populacao_a01;
 
     printf("\n-----------------------------------------------\n\n");
 
@@ -53,31 +57,39 @@ int main() {
     printf("Digite o código da cidade: \n");
     scanf("%s", codigo_a02);
     printf("Digite a população: \n");
-    scanf("%d", &populacao_a02);
+    scanf("%lu", &populacao_a02);
     printf("Digite a área em Km²: \n");
     scanf("%f", &km_area_a02);
     printf("Digite o PIB: \n");
     scanf("%f", &pib_a02);
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &turistico_a02); 
+    scanf("%d", &turistico_a02);
 
-    printf("\n **** **** -CARTA CADASTRADA COM SUCESSO- **** ****  \n\n");
+    //lógica para densidade e pib   
+    densidade_a02 = (float) populacao_a02 / km_area_a02;
+    pib_capta_a02 = (float) pib_a02 / populacao_a02; 
+
+    printf("\n **** **** -CARTA CADASTRADA COM SUCESSO- **** ****  \n");
 
     printf("\nCadastro de cartas: --------------------- Cidade: %s \n\n", estado_B);
 
-    // Cadastro da primeria cidade do estado_A:
+    // Cadastro da primeria cidade do estado_B:
     printf("Digite o nome da 1° cidade de %s: \n", estado_B);
     scanf("%s", cidade_b01);
     printf("Digite o código da cidade: \n");
     scanf("%s", codigo_b01);
     printf("Digite a população: \n");
-    scanf("%d", &populacao_b01);
+    scanf("%lu", &populacao_b01);
     printf("Digite a área em Km²: \n");
     scanf("%f", &km_area_b01);
     printf("Digite o PIB: \n");
     scanf("%f", &pib_b01);
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &turistico_b01);   
+    scanf("%d", &turistico_b01);
+
+    //lógica para densidade e pib   
+    densidade_b01 = (float) populacao_b01 / km_area_b01;
+    pib_capta_b01 = (float) pib_b01 / populacao_b01;   
 
     printf("\n-----------------------------------------------\n\n");
 
@@ -87,7 +99,7 @@ int main() {
     printf("Digite o código da cidade: \n");
     scanf("%s", codigo_b02);
     printf("Digite a população: \n");
-    scanf("%d", &populacao_b02);
+    scanf("%lu", &populacao_b02);
     printf("Digite a área em Km²: \n");
     scanf("%f", &km_area_b02);
     printf("Digite o PIB: \n");
@@ -95,10 +107,30 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &turistico_b02);
 
-    printf("\n **** **** **** V I S U A L I Z A Ç Ã O **** **** ****  \n\n");
+    //lógica para densidade e pib   
+    densidade_b02 = (float) populacao_b02 / km_area_b02;
+    pib_capta_b02 = (float) pib_b02 / populacao_b02; 
 
-    printf("");
+    printf(" **** **** **** V I S U A L I Z A Ç Ã O **** **** ****  \n\n");
+
+    //Estado A
+    printf("Estado: %s \n\n", estado_A);
+    //Cidade 01
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu hab. \nÁrea: %.2f Km² \nPIB: R$ %.1f \nPontos Turísticos: %d \nDensidade Populacional: %.2f hab/km² \nPIB per capta: R$ %.2f \n\n", cidade_a01, codigo_a01, populacao_a01, km_area_a01, pib_a01, turistico_a01, densidade_a01, pib_capta_a01);
+    //Cidade 02
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu hab. \nÁrea: %.2f Km² \nPIB: R$ %.1f \nPontos Turísticos: %d \nDensidade Populacional: %.2f hab/km² \nPIB per capta: R$ %.2f \n\n", cidade_a02, codigo_a02, populacao_a02, km_area_a02, pib_a02, turistico_a02, densidade_a02, pib_capta_a02);
+   
+    printf("\n-----------------------------------------------\n\n");
+    //Estado B
+    printf("Estado: %s \n\n", estado_B);
+    //Cidade 01
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu hab. \nÁrea: %.2f Km² \nPIB: R$ %.1f \nPontos Turísticos: %d \nDensidade Populacional: %.2f hab/km² \nPIB per capta: R$ %.2f \n\n", cidade_b01, codigo_b01, populacao_b01, km_area_b01, pib_b01, turistico_b01, densidade_b01, pib_capta_b01);
+    //Cidade 02
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu hab. \nÁrea: %.2f Km² \nPIB: R$ %.1f \nPontos Turísticos: %d \nDensidade Populacional: %.2f hab/km² \nPIB per capta: R$ %.2f \n\n", cidade_b02, codigo_b02, populacao_b02, km_area_b02, pib_b02, turistico_b02, densidade_b02, pib_capta_b02);
+
+    
     // Comparação de Cartas:
+    
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
 
